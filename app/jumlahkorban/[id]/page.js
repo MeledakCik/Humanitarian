1459f8a4e0789b1ff8d2b797d8662d5857344e87
Kasyaf_SMKN4PADALARANG <kakangkasyaf@gmail.com>
2024-10-15 11:38:40 +0800

    update pengungsi

--- app/jumlahkorban/[id]/page.js
+++ app/jumlahkorban/[id]/page.js
@@ -27,7 +27,7 @@ export default function Sitrep() {
             };
 
             const response = await fetch(
-                formData.id ? "/api/updateJumlahKorban/" : "/api/createJumlahKorban/",
+                formData.id ? "/api/updateJumlahKorban" : "/api/createJumlahKorban",
                 {
                     method: formData.id ? "POST" : "POST",
                     headers: {
@@ -59,13 +59,12 @@ export default function Sitrep() {
         async function fetchData() {
             try {
                 const response = await fetch(`/api/getJumlah_korban?korban_site_id=${id}`);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
+                if (!response.ok) throw new Error('Network response was not ok');
                 const result = await response.json();
                 if (result.status) {
                     const fetchedData = result.data.map((item) => ({
                         id: item.id || "Data tidak ada",
+                        korban_site_id: item.korban_site_id || "Data tidak ada",
                         jumlah: item.jumlah || "Data tidak ada",
                         jeniskorbanjiwa: item.jenis_korban_jiwa || "Data tidak ada",
                     }));
@@ -78,26 +77,22 @@ export default function Sitrep() {
             }
         }
         fetchData();
-        const intervalId = setInterval(fetchData, 1000);
+        const intervalId = setInterval(fetchData, 4000);
         return () => clearInterval(intervalId);
     }, [id]);
 
     const handleDelete = async (itemId) => {
-        if (!itemId) {
-            console.error("ID tidak ditemukan:", itemId);
-            return;
-        }
+        if (!itemId) return;
         try {
             const response = await fetch(`/api/deleteJumlahKorban?id=${itemId}`, {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ id: itemId }), // Pastikan ID ada di body
+                body: JSON.stringify({ id: itemId }),
             });
 
             if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-            const data = await response.json();
             setDataItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
             setMessage("Data berhasil dihapus.");
         } catch (error) {
@@ -108,146 +103,107 @@ export default function Sitrep() {
     const handleEdit = (item) => {
         setFormData({
             id: item.id,
+            korban_site_id: item.korban_site_id,
+            jeniskorbanjiwa: item.jeniskorbanjiwa,
             jumlah: item.jumlah,
-            kerusakan: item.kerusakan,
-            satuan: item.satuan,
-            dampak_site_id: id,
         });
         setShowForm(true);
     };
 
-    useEffect(() => {
-        if (id) {
-            setFormData((prevFormData) => ({
-                ...prevFormData,
-                korban_site_id: id,
-            }));
-        }
-    }, [id]);
-
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setFormData({
-            ...formData,
+        setFormData((prevData) => ({
+            ...prevData,
             [name]: value,
-        });
+        }));
     };
-    return (
-        <>
-            <div className="flex flex-col items-center bg-white">
-                <nav className="w-full bg-[#ff6b00] p-6 shadow-b-lg">
-                    <div className="flex mt-[10px] justify-center">
-                        <p className="text-white font-bold text-[22px]">Jumlah Korban</p>
-                    </div>
-                </nav>
-                <div className="w-[380px] max-w-md mt-4">
-                    <button
-                        className="w-full h-[50px] bg-green-500 rounded-lg text-white font-bold flex items-center justify-center mb-4"
-                        onClick={() => setShowForm(!showForm)}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-2"
-                        >
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
-                        </svg>
-                        {showForm ? "Tutup Data" : "Tambahkan Data"}
-                    </button>
 
-                    {showForm && (
-                        <div className="mt-[10px] bg-white rounded-lg" onSubmit={handleSubmit}>
-                            <div className="mb-4">
-                                <label className="block text-[14px] font-bold text-gray-700">Jumlah Korban Site ID*</label>
-                                <div className="relative">
-                                    <input
-                                        type="text"
-                                        name="korban_site_id"
-                                        value={formData.korban_site_id}
-                                        onChange={handleInputChange}
-                                        className="mt-1 block w-full p-2 border border-orange-500 rounded-md focus:outline-none"
-                                        readOnly
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-[14px] font-bold text-gray-700">Jenis Korban Jiwa*</label>
-                                <div className="relative">
-                                    <input
-                                        type="text"
-                                        name="jeniskorbanjiwa"
-                                        value={formData.jeniskorbanjiwa}
-                                        onChange={handleInputChange}
-                                        className="mt-1 block w-full p-2 border border-orange-500 rounded-md focus:outline-none"
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-[14px] font-bold text-gray-700">Jumlah*</label>
-                                <div className="relative">
-                                    <input
-                                        type="number"
-                                        name="jumlah"
-                                        value={formData.jumlah}
-                                        onChange={handleInputChange}
-                                        className="mt-1 block w-full p-2 border border-orange-500 rounded-md focus:outline-none"
-                                    />
-                                </div>
-                            </div>
-                            <div className="flex justify-between">
-                                <Link href="../lokasiterdampak" passHref>
-                                    <button className="w-[100px] h-[40px] bg-white border border-orange-500 font-bold text-black rounded-lg">
-                                        BACK
-                                    </button>
-                                </Link>
-                                <button
-                                    type="submit"
-                                    onClick={handleSubmit}
-                                    className="w-[100px] h-[40px] bg-[#ff6b00] font-bold text-white rounded-lg"
-                                >
-                                    SAVE
+    return (
+        <div className="flex flex-col items-center bg-white">
+            <nav className="w-full bg-[#ff6b00] p-6 shadow-b-lg">
+                <div className="flex mt-[10px] justify-center">
+                    <p className="text-white font-bold text-[22px]">Jumlah Korban</p>
+                </div>
+            </nav>
+            <div className="w-[380px] max-w-md mt-4">
+                <button
+                    className="w-full h-[50px] bg-green-500 rounded-lg text-white font-bold flex items-center justify-center mb-4"
+                    onClick={() => setShowForm(!showForm)}
+                >
+                    {showForm ? "Tutup Data" : "Tambahkan Data"}
+                </button>
+
+                {showForm && (
+                    <form className="mt-[10px] bg-white rounded-lg" onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-[14px] font-bold text-gray-700">Jumlah Korban Site ID*</label>
+                            <input
+                                type="text"
+                                name="korban_site_id"
+                                value={formData.korban_site_id}
+                                onChange={handleInputChange}
+                                className="mt-1 block w-full p-2 border border-orange-500 rounded-md focus:outline-none"
+                                readOnly
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-[14px] font-bold text-gray-700">Jenis Korban Jiwa*</label>
+                            <input
+                                type="text"
+                                name="jeniskorbanjiwa"
+                                value={formData.jeniskorbanjiwa}
+                                onChange={handleInputChange}
+                                className="mt-1 block w-full p-2 border border-orange-500 rounded-md focus:outline-none"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-[14px] font-bold text-gray-700">Jumlah*</label>
+                            <input
+                                type="number"
+                                name="jumlah"
+                                value={formData.jumlah}
+                                onChange={handleInputChange}
+                                className="mt-1 block w-full p-2 border border-orange-500 rounded-md focus:outline-none"
+                            />
+                        </div>
+                        <div className="flex justify-between">
+                            <Link href="../lokasiterdampak" passHref>
+                                <button type="button" className="w-[100px] h-[40px] bg-white border border-orange-500 font-bold text-black rounded-lg">
+                                    BACK
                                 </button>
+                            </Link>
+                            <button type="submit" className="w-[100px] h-[40px] bg-[#ff6b00] font-bold text-white rounded-lg">
+                                {formData.id ? "UPDATE" : "SAVE"}
+                            </button>
+                        </div>
+                    </form>
+                )}
+
+                <div className="mt-7">
+                    {dataItems.map((item, index) => (
+                        <div key={index} className="p-4 mt-4 bg-orange-100 border border-orange-500 rounded-lg shadow-md">
+                            <div className="flex justify-between">
+                                <div className="w-1/2">
+                                    <p className="font-bold text-gray-700 text-md">Jenis Korban Jiwa</p>
+                                    <p className="text-gray-800">{item.jeniskorbanjiwa}</p>
+                                </div>
+                                <div className="w-1/2">
+                                    <p className="font-bold text-gray-700 text-md">Jumlah</p>
+                                    <p className="text-gray-800">{item.jumlah}</p>
+                                </div>
+                                <div className="flex">
+                                    <button className="mr-1 text-blue-500 hover:text-blue-700" onClick={() => handleEdit(item)}>
+                                        Edit
+                                    </button>
+                                    <button className="text-red-500 hover:text-red-700" onClick={() => handleDelete(item.id)}>
+                                        Hapus
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    )}
-
-                    <div className="mt-7">
-                        {dataItems.map((item, index) => (
-                            <div
-                                key={index}
-                                className="p-4 mt-4 bg-orange-100 border border-orange-500 rounded-lg shadow-md"
-                            >
-                                <div className="flex justify-between">
-                                    <div className="w-1/2">
-                                        <p className="font-bold text-gray-700 text-md">Jenis Korban Jiwa</p>
-                                        <p className="text-gray-800">{item.jeniskorbanjiwa}</p>
-                                    </div>
-                                    <div className="w-1/2">
-                                        <p className="font-bold text-gray-700 text-md">Jumlah</p>
-                                        <p className="text-gray-800">{item.jumlah}</p>
-                                    </div>
-                                    <div className="flex">
-                                        <button className="mr-1 text-blue-500 hover:text-blue-700">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-4M16 3h-4v2h4V3z" />
-                                            </svg>
-                                            Edit
-                                        </button>
-                                        <button className="text-red-500 hover:text-red-700" onClick={() => handleDelete(item.id)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M10 3h4a1 1 0 011 1v1H9V4a1 1 0 011-1z" />
-                                            </svg>
-                                            Hapus
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
